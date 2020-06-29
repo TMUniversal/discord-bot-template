0033809b0344e301e7222d72eca42609e77ec949
Universal Studio <uni@un1.pw>
2020-06-29 18:01:40 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-discord bot template
+# Discord Bot Template (TypeScript)
+
+## Installation
+
+Run `npm install`
+
+## Setup
+
+- Make a copy of `data.example.json`, name it `data.json`.
+- Fill in the necessary values, remove the comment (since comments are not supported in JSON).
+  - `owners` may be an array of strings
+
+## Starting
+
+To start the bot, it must first be complied.
+
+- Run `npm run build`
+- You may then start with `npm start` or, if you have pm2 installed: `pm2 start pm2-start.json`
+- Alternatively: Run `npm run cs` to build and then start.
+
+`npm run startmon` will launch the bot in monitor mode, i.e. it will reload anytime you save a file.
+
+# Credits
+
+Credits to [Hydractify](https://github.com/Hydractify/kanna_kobayashi) for their logging system.
---
+++ src/structures/WebhookLogger.ts
@@ -0,0 +1,53 @@
+/* eslint-disable no-unused-vars */
+/**
+ * @author Hydractify
+ * @see https://github.com/Hydractify/kanna_kobayashi
+ */
+import { MessageAttachment, WebhookClient, WebhookMessageOptions } from 'discord.js'
+
+import { colors, LogLevel } from '../types/LogLevel'
+import { Logger } from './Logger'
+import { MessageEmbed } from './MessageEmbed'
+import configFile from '../config'
+
+const webhook: WebhookClient = new WebhookClient(configFile.webhook.id, configFile.webhook.secret)
+
+export class WebhookLogger extends Logger {
+  protected static _instance: WebhookLogger;
+
+  public static get instance (): WebhookLogger {
+    return this._instance || new this()
+  }
+
+  private _webhookLevel: LogLevel = LogLevel.SILLY;
+
+  protected _write (level: LogLevel, tag: string, data: any[]): void {
+    super._write(level, `Webhook][${tag}`, data)
+    if (this._webhookLevel < level) return
+
+    const cleaned: string = this._prepareText(data)
+
+    const embed: MessageEmbed = new MessageEmbed()
+      .setTimestamp()
+      .setColor(colors[level][2])
+      .setFooter(this._processTag)
+    const options: WebhookMessageOptions = {
+      avatarURL: 'https://vignette.wikia.nocookie.net/avatar/images/1/1f/Joo_Dee.png/revision/latest?cb=20140422090643',
+      embeds: [embed],
+      username: 'Emoji Stealer Status'
+    }
+
+    if (cleaned.length <= 2048) {
+      embed.setDescription(cleaned)
+    } else {
+      embed.setDescription('Data is too long, falling back to file.')
+      options.files = [new MessageAttachment(Buffer.from(cleaned), 'file.txt')]
+    }
+
+    if (tag) embed.setTitle(tag)
+
+    webhook.send(options)
+    // Message is still in the console
+      .catch(() => undefined)
+  }
+}
